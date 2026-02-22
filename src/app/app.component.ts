632d9cec6c60931e1781da7ddab40168e8f8480e
Harshad Porajwar <harshadporajwar21@gmail.com>
2026-02-22 23:52:22 +0530

    components and click in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class App {
-  protected readonly title = signal('Angular_tutorial_app');
+export class AppComponent {
+    // name = "Harshad Porajwar";
+    // user1 = "samu";
+    // user2 = "samu"
+
+    handleClickEvent(){
+      console.log("Function called");
+      this.OtherFunCall();
+    }
+
+    OtherFunCall(){
+      console.log("Other Function called");
+    }
+
+    name:string = "Harshad"
+    data:any= "2005"
+    age : string|number = 21
+
+    UpdateName(){
+      this.name = "samu"
+      this.data = 2008
+      this.age = 17
+      console.log(this.name,this.data,this.age);
+    }
+
 }
+
+
+
